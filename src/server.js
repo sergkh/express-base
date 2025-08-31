593a966c802f08a68496a7908455cedae8fa760a
Sergey Khruschak <sergey.khruschak@fidesmo.com>
2025-08-31 12:52:47 +0300

    body parsing

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 const app = express()
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
